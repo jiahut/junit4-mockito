e5f2c6f2787600a9770dd4df2a9557863440b261
zhijia,.zhang <jiahut@gmail.com>
2019-03-14 17:41:25 +0800

    init

---
+++ src/TestMockFinal.java
@@ -0,0 +1,18 @@
+import static org.mockito.Mockito.*;
+
+final class FinalClass {
+  int m1() {
+    throw new RuntimeException("42");
+  }
+}
+
+public class TestMockFinal {
+  public static void main(String[] args) {
+    FinalClass cl = mock(FinalClass.class);
+    when(cl.m1()).thenReturn(-1);
+    if (cl.m1() != -1) {
+      throw new Error("Not mocked!");
+    }
+    System.out.println("ok");
+  }
+}
